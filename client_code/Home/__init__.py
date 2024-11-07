7a08cfffbfcd144f022164c6a8a80578a6c7b885
jbrow57@gmail.com <jbrow57@gmail.com>
2024-11-07 14:11:23 +0000

    Edited Home

--- client_code/Home/__init__.py
+++ client_code/Home/__init__.py
@@ -3,7 +3,9 @@ from anvil import *
 import anvil.server
 from ..About import About
 from ..Contact import Contact
+from anvil.js.window import document
 
+#document.addEventListener("click", btn_int_click)
 
 class Home(HomeTemplate):
     def __init__(self, **properties):
@@ -12,15 +14,23 @@ class Home(HomeTemplate):
 
         # Any code you write here will run before the form opens.
         # label and button added dynamically
-        btn_int = Button(text="Click Me")
+
+        btn_on = Button(text="Click Me")
         lp = LinearPanel(background="lightgreen")
         lp.add_component(Label(text="Hello"))
-        lp.add_component(btn_int)
+        lp.add_component(btn_on)
         self.layout.add_component(lp)
+        
+       
 
-    def btn_int_click(self, **event_args):
-        """ dynamic button ???"""
-        alert("The External Button was clicked")
+    def btn_on_click(self, **event_args):
+        """ dynamic button ???"""  
+        self.xname = self.text_box_name.text
+        self.xemail = self.text_box_email.text
+        self.position = self.text_box_position.text
+        Notification(f"Name: {self.xname}\nEmail: {self.xemail}").show()
+        
+    document.addEventListener("click", btn_on_click)
 
     def navigation_link_about_click(self, **event_args):
         """This method is called when the component is clicked"""
@@ -28,6 +38,4 @@ class Home(HomeTemplate):
         open_form(form)
         pass
 
-    def button_submit_click(self, **event_args):
-        """This method is called when the component is clicked."""
-        alert("Internal Button Clicked")
+   
