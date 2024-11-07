5955d3f72ec01e8ef6ef25fe2ad4e6b3fc36c9d6
jbrow57@gmail.com <jbrow57@gmail.com>
2024-11-07 16:14:32 +0000

    Created Form1; Edited Home

--- client_code/Home/__init__.py
+++ client_code/Home/__init__.py
@@ -1,11 +1,8 @@
 from ._anvil_designer import HomeTemplate
 from anvil import *
 import anvil.server
-from ..About import About
-from ..Contact import Contact
-from anvil.js.window import document
-
-#document.addEventListener("click", btn_int_click)
+#from ..About import About
+#from ..Contact import Contact
 
 class Home(HomeTemplate):
     def __init__(self, **properties):
@@ -15,27 +12,24 @@ class Home(HomeTemplate):
         # Any code you write here will run before the form opens.
         # label and button added dynamically
 
-        btn_on = Button(text="Click Me")
-        lp = LinearPanel(background="lightgreen")
-        lp.add_component(Label(text="Hello"))
-        lp.add_component(btn_on)
-        self.layout.add_component(lp)
         
-       
-
-    def btn_on_click(self, **event_args):
-        """ dynamic button ???"""  
-        self.xname = self.text_box_name.text
-        self.xemail = self.text_box_email.text
-        self.position = self.text_box_position.text
-        Notification(f"Name: {self.xname}\nEmail: {self.xemail}").show()
+        #lp = LinearPanel(background="lightgreen")
+        #lp.add_component(Label(text="Hello"))
+        #lp.add_component(btn_on)
+        #self.layout.add_component(lp)
         
-    document.addEventListener("click", btn_on_click)
 
     def navigation_link_about_click(self, **event_args):
         """This method is called when the component is clicked"""
         form = About()
         open_form(form)
+        pass
+
+    def button_submit_click(self, **event_args):
+        """This method is called when the component is clicked."""
+        #name = self.text_box_name.text
+        #email = self.text_box_email.text
+        Notification(f"Name: {self.text_box_name.text}\nEmail: {self.text_box_email.text}")
         pass
 
    
